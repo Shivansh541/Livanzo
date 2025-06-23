7e63385c5f67264451013d7559f57bdf935ae084
Shivansh Singh Rathore <rathoreshivansh54@gmail.com>
2025-06-23 16:45:15 +0530

    version 1.1.3

--- hostelhub/src/components/user/UserNavbar.js
+++ hostelhub/src/components/user/UserNavbar.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import UserRightNav from './UserRightNav';
 import UserNavLinks from './UserNavLinks';
 
 const UserNavbar = ({ searchTerm, setSearchTerm }) => {
+    const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme') || 'light';
+    setTheme(storedTheme);
+  }, []);
   return (
     <nav className='userNavbar'>
     <div className="left-nav">
-      <p className="logo">Livanzo</p>
+      <img src={theme === "light"?"/logo light.png":"/logo dark.png"} alt=""/>
       <div className="navlinks">
         <UserNavLinks/>
       </div>
